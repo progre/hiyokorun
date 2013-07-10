5902def0b5cc9fd67b527e1724e3e17edc8d9d2c
progre <djyayutto@gmail.com>
2013-07-10 23:13:20 +0900

    fix

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -1,6 +1,5 @@
 /// <reference path='../../../lib/DefinitelyTyped/easeljs/easeljs.d.ts'/>
 /// <reference path='../../../lib/EventEmitter.d.ts'/>
-/// <reference path='../../../lib/linq.d.ts'/>
 
 declare var stats;
 
